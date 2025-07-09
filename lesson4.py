8f14023e6b9e4eee1d774902db585ba135473e24
Wozewald <wrongrabbitqwerty@gmail.com>
2025-07-09 14:48:35 +0300

    второе дз исправления

--- lesson4.py
+++ lesson4.py
@@ -1,5 +1,27 @@
 # Пятое
+from py_base import value
 
+first_list = [-10, 15, 30, -70, -150]
 
+def filter_positive(numbers):
+    positive_numbers = [num for num in numbers if num > 0]
+    return positive_numbers
+
+result = filter_positive(first_list)
+print(result)
 
 # Шестое
+
+student = {
+    "Имя": "Мария",
+    "Возраст": 21
+}
+
+inc = 3
+
+def update_dict(student, inc):
+    student["Возраст"] += inc
+    return student
+
+students = update_dict(student, inc)
+print(students.get("Возраст"))
